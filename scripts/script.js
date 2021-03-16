49826d33c4d93e3383d64d45eab47c481d6ed1df
Shilachkashila <67004521+Shilachkashila@users.noreply.github.com>
2021-03-16 21:55:13 +0200

    accordion

--- scripts/script.js
+++ scripts/script.js
@@ -42,7 +42,18 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     })
-
   };
+
+  const accordion () => {
+    const characteristicsTitle = document.querySelectorAll('.characteristics__title');
+    const characteristicsDescription = document.querySelectorAll('.characteristics__description');
+
+    characteristicsTitle.forEach((elem, i) => {
+      elem.addEventListener('click', () => {
+        elem.classList.toggle('active');
+      })
+    });
+
+  }
   tabs();
 });
